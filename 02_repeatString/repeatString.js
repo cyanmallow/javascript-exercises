a52d97f00b452a7441562240e317ba407fd49aba
cyanmallow <ggmmallow@gmail.com>
2024-03-19 12:30:50 +0700

    finished 1, 2, 3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(inputString, howManyTimes) {
+    let result= '';
+    if (howManyTimes < 0){
+        return 'ERROR'
+    } else {
+        for (let i=0; i<howManyTimes; i++){
+            result +=  inputString;
+        }
+        return result;
+    }
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(normalString) {
+    const lettersArray = normalString.split(' ');
 
+    const reversedArray = lettersArray.map(word => {
+        return word.split('').reverse().join('');
+    })
+    return reversedArray.reverse().join(' ');
 };
 
 // Do not edit below this line
