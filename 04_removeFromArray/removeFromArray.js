c1e826d428a273abef49c1623a54213985e06423
Ginger <ggmmallow@gmail.com>
2024-03-29 16:08:11 +0700

    did the first half of removeFromArray, not on my computer so have to commit

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(originalArray, removeWhichNumber) {
+    const index = originalArray.indexOf(removeWhichNumber);
+    if (index !== -1){
+        originalArray.splice(index, 1);
+    }
+    return originalArray;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
